a7548b59be5674ee35116d5e6f54c21ff16635b1
stefanoduca <s.duca.2665@vallauri.edu>
2025-05-06 08:38:36 +0200

    fix

--- dist/server.js
+++ dist/server.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const express_1 = __importDefault(require("express"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 const DB_NAME = "RilieviPerizie";
@@ -44,12 +45,30 @@ app.use('*', (req, res, next) => {
     console.log(req.method + ': ' + req.originalUrl);
     next();
 });
+const whitelist = ['http://localhost:3000', 'http://localhost:4200', 'https://rilievieperizieduca.onrender.com', 'http://localhost:8100'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+        const trimmedOrigin = origin.trim();
+        console.log("Request from origin:", trimmedOrigin);
+        if (whitelist.includes(trimmedOrigin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS: " + trimmedOrigin));
+        }
+    }
+};
+app.use((0, cors_1.default)(corsOptions));
 //Client routes
 app.post('/autentica', (req, res, next) => {
     getCollection('Utenti').then((collection) => {
         collection.findOne({ codice_operatore: req.body.username, password: req.body.password }, {
             projection: {
-                password: 0
+                password: 0,
+                token: 0
             }
         }).then((data) => {
             if (data) {
@@ -156,3 +175,99 @@ app.post('/addOperatore', (req, res) => __awaiter(void 0, void 0, void 0, functi
         });
     });
 }));
+// API
+app.post('/api/autentica', (req, res) => {
+    getCollection('Utenti').then((collection) => {
+        collection.findOne({ codice_operatore: req.body.username, password: req.body.password }, {
+            projection: {
+                password: 0,
+            }
+        }).then((data) => {
+            if (data) {
+                res.send({ result: true, data: data });
+            }
+            else {
+                res.status(401).send({ result: false, data: 'Unauthorized' });
+            }
+        }).catch((err) => {
+            console.error(err);
+            res.status(500).send({ result: false, data: 'Error retrieving user' });
+        });
+    }).catch((err) => {
+        res.status(500).send({ result: false, data: 'Unauthorized MongoDB: ' + err });
+    });
+});
+app.post('/api/cambia-password', (req, res) => {
+    if (!req.headers['authorization'].length) {
+        res.status(401).send({ result: false, data: 'Unauthorized' });
+        return;
+    }
+    const token = req.headers['authorization'].split(' ')[1];
+    getCollection('Utenti').then((collection) => {
+        collection.updateOne({ token: token, password: req.body.password }, { $set: { password: req.body.new_password, primo_accesso: false } }).then((data) => {
+            if (data.modifiedCount > 0) {
+                res.send({ result: true, data: data });
+            }
+            else {
+                res.status(400).send({ result: false, data: 'Bad Request' });
+            }
+        }).catch((err) => {
+            console.error(err);
+            res.status(500).send({ result: false, data: 'Error retrieving user' });
+        });
+    }).catch((err) => {
+        res.status(500).send({ result: false, data: 'Unauthorized MongoDB: ' + err });
+    });
+});
+app.post('/api/upload-perizia', (req, res) => {
+    if (!req.headers['authorization'].length) {
+        res.status(401).send({ result: false, data: 'Unauthorized' });
+        return;
+    }
+    const token = req.headers['authorization'].split(' ')[1];
+    getCollection('Utenti').then((collection) => {
+        collection.findOne({ token: token }, {
+            projection: {
+                password: 0,
+            }
+        }).then((utente) => {
+            getCollection('Perizie').then((collection) => {
+                const today = new Date();
+                const year = today.getFullYear();
+                const month = String(today.getMonth() + 1).padStart(2, '0');
+                const day = String(today.getDate()).padStart(2, '0');
+                const datePart = `${year}${month}${day}`;
+                collection.find({ codice_perizia: { $regex: `^PZ${datePart}-` } })
+                    .project({ codice_perizia: 1 })
+                    .sort({ codice_perizia: -1 })
+                    .limit(1)
+                    .toArray()
+                    .then((results) => {
+                    let lastNumber = 0;
+                    if (results.length > 0) {
+                        lastNumber = parseInt(results[0].codice_perizia.split('-')[1], 10);
+                    }
+                    const codice_perizia = `PZ${datePart}-${String(lastNumber + 1).padStart(3, '0')}`;
+                    const data = Object.assign(req.body, { codice_operatore: utente.codice_operatore, codice_perizia });
+                    collection.insertOne(data).then((insertResult) => {
+                        if (insertResult) {
+                            res.send({ result: true, data: insertResult });
+                        }
+                        else {
+                            res.status(401).send({ result: false, data: 'Unauthorized' });
+                        }
+                    }).catch((err) => {
+                        console.error(err);
+                        res.status(500).send({ result: false, data: 'Error inserting perizia' });
+                    });
+                }).catch((err) => {
+                    console.error(err);
+                    res.status(500).send({ result: false, data: 'Error generating codice_perizia' });
+                });
+            });
+        }).catch((err) => {
+            console.error(err);
+            res.status(500).send({ result: false, data: 'Error retrieving user' });
+        });
+    });
+});
